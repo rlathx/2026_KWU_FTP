1a322eb0cdbb8edccf929ae409dc39f76ab8a100
rlathx <kimth101550@gmail.com>
2026-03-29 21:46:12 +0900

    Assignment1_2

--- FTP2_ls/kw2024402034_ls.c
+++ FTP2_ls/kw2024402034_ls.c
@@ -1,24 +1,50 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <errno.h>
 
 int main(int argc, char *argv[]) {
-    DIR *dp;
-    struct dirent *dirp;
+    DIR* dp; 
+    struct dirent* dirp;
+    char* path;
 
-    // 디렉토리 열기 (인자 없으면 현재 디렉토리)
-    if (argc == 1) {
-        dp = opendir(".");
-    } else {
-        dp = opendir(argv[1]);
+    // error 1. 인자 개수 초과
+    if (argc > 2) {
+        printf("only one directory path can be processed\n");
+
+        return 0;
     }
 
-    // 디렉토리 순회
+    // 대상 디렉토리 설정 (인자가 없으면 현재 디렉토리 ".")
+    if (argc == 1) {
+        path = ".";
+    } else {
+        path = argv[1];
+    }
+
+    // 디렉토리 열기 
+    dp = opendir(path);
+
+    // error 2. 존재하지 않는 dir
+    // error 3. 접근 권한 없음
+    if (dp == NULL) {
+        if (errno == ENOENT) { // 파일이나 디렉토리가 없는 경우
+            printf("%s: cannot access '%s': No such directory\n", argv[0], path);
+        } 
+        else if (errno == EACCES) { // 권한이 거부된 경우 (Permission denied)
+            printf("%s: cannot access '%s': Access denied\n", argv[0], path);
+        } 
+
+        closedir(dp);
+        return 0;
+    }
+
+    // 디렉토리 순회 및 파일 이름 출력 (File names only)
     while ((dirp = readdir(dp)) != NULL) {
         printf("%s\n", dirp->d_name);
     }
 
-    // 디렉토리 닫기
+    // 5. 디렉토리 닫기
     closedir(dp);
 
     return 0;
