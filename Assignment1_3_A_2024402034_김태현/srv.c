c762a0d303a70319edd7bd9ebb91ca0e60ace24d
rlathx <kimth101550@gmail.com>
2026-04-11 22:50:58 +0900

    without ls/dir/get/put

--- Assignment1_3_A_2024402034_김태현/srv.c
+++ Assignment1_3_A_2024402034_김태현/srv.c
@@ -24,35 +24,111 @@ int main(int argc, char **argv){
 
     }
     else if (strncmp(FTPcommand, "PWD", 3) == 0) {
-        // [PWD 처리] 현재 경로 출력 로직 예정
-        write(STDOUT_FILENO, "Processing PWD...\n", 18);
+        char currentWD[MAX_BUF];
+
+        // [error - PWD] return NULL 대비
+        if(getcwd(currentWD, sizeof(currentWD)) == NULL){
+            char* error_msg = "Error: directory not found\n";
+            write(STDOUT_FILENO, error_msg, strlen(error_msg));
+
+            exit(1);
+        }
+
+        write(STDOUT_FILENO, "\'", 2);
+        write(STDOUT_FILENO, currentWD, strlen(currentWD));
+        write(STDOUT_FILENO, "\'", 2);
+        write(STDOUT_FILENO, " is current directory", 22);
     }
     else if (strncmp(FTPcommand, "CWD", 3) == 0) {
-        // [CWD/CDUP 처리] 디렉토리 이동 로직 예정
-        write(STDOUT_FILENO, "Processing CD...\n", 17);
+        strtok(FTPcommand, " ");
+        char *targetWD = strtok(NULL, " ");
+
+        // [error - CWD] 경로명 틀림
+        if(targetWD == NULL || chdir(targetWD) == -1){
+            char* error_msg = "Error: directory not found\n";
+            write(STDOUT_FILENO, error_msg, strlen(error_msg));
+
+            exit(1);
+        }
+
+        write(STDOUT_FILENO, "\'", 2);
+        write(STDOUT_FILENO, targetWD, strlen(targetWD));
+        write(STDOUT_FILENO, "\'", 2);
+        write(STDOUT_FILENO, " is current directory", 22);
     }
     else if(strncmp(FTPcommand, "CDUP", 4) == 0){
+        if(chdir("..") == -1){
+            char* error_msg = "Error: directory not found\n";
+            write(STDOUT_FILENO, error_msg, strlen(error_msg));
 
+            exit(1);
+        }
+
+        char currentWD[MAX_BUF];
+
+        // [error - CDUP] return NULL 대비
+        if(getcwd(currentWD, sizeof(currentWD)) == NULL){
+            char* error_msg = "Error: directory not found\n";
+            write(STDOUT_FILENO, error_msg, strlen(error_msg));
+
+            exit(1);
+        }
+
+        write(STDOUT_FILENO, "\'", 2);
+        write(STDOUT_FILENO, currentWD, strlen(currentWD));
+        write(STDOUT_FILENO, "\'", 2);
+        write(STDOUT_FILENO, " is current directory", 22);
     }
     else if (strncmp(FTPcommand, "MKD", 3) == 0) {
-        // [MKD 처리] 디렉토리 생성 로직 예정
-        write(STDOUT_FILENO, "Processing MKDIR...\n", 20);
+        char* dirName = strtok(FTPcommand, " ");
+
+        while((dirName = strtok(NULL, " ")) != NULL){
+            if(mkdir(dirName, 0775) == -1){
+                write(STDOUT_FILENO, "Error: cannot create directory \'", 33);
+                write(STDOUT_FILENO, dirName, strlen(dirName));
+                write(STDOUT_FILENO, "\': File exists\n", 16);
+            }
+        }
     }
     else if (strncmp(FTPcommand, "DELE", 4) == 0) {
-        // [DELE 처리] 파일 삭제 로직 예정
-        write(STDOUT_FILENO, "Processing DELETE...\n", 21);
+        char* dirName = strtok(FTPcommand, " ");
+
+        while((dirName = strtok(NULL, " ")) != NULL){
+            if(unlink(dirName) == -1){
+                write(STDOUT_FILENO, "Error: failed to delete file\n", 45);
+            }
+        }
     }
     else if (strncmp(FTPcommand, "RMD", 3) == 0) {
-        // [RMD 처리] 디렉토리 삭제 로직 예정
-        write(STDOUT_FILENO, "Processing RMDIR...\n", 20);
+        char* dirName = strtok(FTPcommand, " ");
+
+        while((dirName = strtok(NULL, " ")) != NULL){
+            if(rmdir(dirName) == -1){
+                write(STDOUT_FILENO, "Error: failed to remove \'", 26);
+                write(STDOUT_FILENO, dirName, strlen(dirName));
+                write(STDOUT_FILENO, "\'\n", 3);
+            }
+        }
     }
     else if (strncmp(FTPcommand, "RNFR", 4) == 0) {
-        // [RNFR/RNTO 처리] 이름 변경/위치 이동 로직 예정
-        write(STDOUT_FILENO, "Processing RENAME...\n", 21);
+        strtok(FTPcommand, " ");
+        char* oldname = strtok(NULL, " ");
+        strtok(NULL, " ");
+        char* newname = strtok(NULL, " ");
+
+        if (oldname == NULL || newname == NULL) {
+            char* error_msg = "Error: argument is required\n";
+            write(STDOUT_FILENO, error_msg, strlen(error_msg));
+            exit(1);
+        }
+
+        // [error - RNFR&RNTO] 이미 있는 이름으로 변경 요청
+        if (rename(oldname, newname) == -1){
+            write(STDOUT_FILENO, "Error: name to change already exists\n", 38);
+            exit(1);
+        }
     }
     else if (strncmp(FTPcommand, "RETR", 4) == 0) {
-        // [RETR 처리] 파일 전송(다운로드) 로직 예정
-        write(STDOUT_FILENO, "Processing GET...\n", 18);
     }
     else if (strncmp(FTPcommand, "STOR", 4) == 0) {
         // [STOR 처리] 파일 수신(업로드) 로직 예정
